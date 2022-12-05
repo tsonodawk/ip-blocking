71c978b113546a72f9fac1c2a48b8c6dbb410388
Toshiro Sonoda <t.sonoda.wk@gmail.com>
2022-12-05 18:11:12 +0900

    ip変更

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { blockedIp } from "@lib/rules/ip";
-import getIP from "@lib/get-ip";
+import { NextApiRequest } from "next";
+// import getIP from "@lib/get-ip";
+
+const getIP = (request: Request | NextApiRequest) => {
+  const xff =
+    request instanceof Request
+      ? request.headers.get("x-forwarded-for")
+      : request.headers["x-forwarded-for"];
+
+  return xff ? (Array.isArray(xff) ? xff[0] : xff.split(",")[0]) : "127.0.0.1";
+};
 
 export const config = {
   matcher: ["/blocked", "/am-i-blocked"],
@@ -9,11 +19,11 @@ export const config = {
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  const blockIpList = ["153.127.197.219"];
+  const blockIpList = ["124.215.13.29", , "127.0.0.1"];
 
   console.log(`URLは：${url}`);
   const reqIp = getIP(req);
-  console.log(`IPは：${url}`);
+  console.log(`IPは：${reqIp}`);
 
   if (blockIpList.includes(reqIp)) {
     url.pathname = "/blocked";
@@ -21,13 +31,13 @@ export async function middleware(req: NextRequest) {
   }
 
   // Rewrite to /blocked if the IP is blocked
-  if (url.pathname === "/am-i-blocked") {
-    if (await blockedIp(req)) {
-      url.pathname = "/blocked";
-      return NextResponse.rewrite(url);
-    }
-    return;
-  }
+  // if (url.pathname === "/am-i-blocked") {
+  // if (await blockedIp(req)) {
+  //   url.pathname = "/blocked";
+  //   return NextResponse.rewrite(url);
+  // }
+  //   return;
+  // }
 
   // Trying to access the /blocked page manually is disallowed
   if (url.pathname === "/blocked") {
