8f9665c9ae21f4f225067c9dd13142e3933e638f
Toshiro Sonoda <t.sonoda.wk@gmail.com>
2022-12-05 12:45:34 +0900

    test-console追加

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,31 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import { blockedIp } from '@lib/rules/ip'
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { blockedIp } from "@lib/rules/ip";
+import getIP from "@lib/get-ip";
 
 export const config = {
-  matcher: ['/blocked', '/am-i-blocked'],
-}
+  matcher: ["/blocked", "/am-i-blocked"],
+};
 
 export async function middleware(req: NextRequest) {
-  const url = req.nextUrl
+  const url = req.nextUrl;
+
+  console.log(`URLは：${url}`);
+  const reqIp = getIP(req);
+  console.log(`IPは：${url}`);
 
   // Rewrite to /blocked if the IP is blocked
-  if (url.pathname === '/am-i-blocked') {
+  if (url.pathname === "/am-i-blocked") {
     if (await blockedIp(req)) {
-      url.pathname = '/blocked'
-      return NextResponse.rewrite(url)
+      url.pathname = "/blocked";
+      return NextResponse.rewrite(url);
     }
-    return
+    return;
   }
 
   // Trying to access the /blocked page manually is disallowed
-  if (url.pathname === '/blocked') {
-    url.pathname = `/404`
-    return NextResponse.rewrite(url)
+  if (url.pathname === "/blocked") {
+    url.pathname = `/404`;
+    return NextResponse.rewrite(url);
   }
 }
